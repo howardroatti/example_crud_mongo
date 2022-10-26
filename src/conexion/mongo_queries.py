e07663154c2ce04bda30adad9a65a343ef349c84
Howard Roatti <hroatti@hotmail.com>
2022-10-26 20:24:07 -0300

    Fix connection credentials

--- src/conexion/mongo_queries.py
+++ src/conexion/mongo_queries.py
@@ -6,10 +6,8 @@ class MongoQueries:
         self.port = 27017
         self.service_name = 'labdatabase'
 
-        # with open("conexion/passphrase/authentication.mongo", "r") as f:
-        #     self.user, self.passwd = f.read().split(',')
-        self.user = "labdatabase"
-        self.passwd = "labDatabase2022"
+        with open("conexion/passphrase/authentication.mongo", "r") as f:
+            self.user, self.passwd = f.read().split(',')
 
     def __del__(self):
         if hasattr(self, "mongo_client"):
